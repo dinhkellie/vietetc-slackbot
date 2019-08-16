92a39ca4c8b9e95af215953e94bcf5f71f6c7843
Kellie Dinh <dinhkellie@yahoo.com>
2019-08-16 08:53:51 +0700

    rename files

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 import os
 from flask import Flask, request, make_response, render_template
-from Startbot import *
+from startbot import Startbot
 
 app = Flask(__name__)
 @app.route("/", methods=["GET"])
